37d646651a619703cd08eed3378114dc31fee4e0
Luiz Lemos - Montreal Clube de Hospedagem <m0864@CLUBEMONTREAL.COM.BR>
2019-09-23 17:23:44 -0300

    Iniciando projeto, login pronto. LogOut +/-

---
+++ public/js/app.js
@@ -0,0 +1,55 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCw2DUCoUfB5R8VUrsVbTxZvghe1v8q6DE",
+    authDomain: "cash-save.firebaseapp.com",
+    databaseURL: "https://cash-save.firebaseio.com",
+    projectId: "cash-save",
+    storageBucket: "",
+    messagingSenderId: "697816366797",
+    appId: "1:697816366797:web:7086cff55523b9e2633384",
+    measurementId: "G-MJZXVTZ7K9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        if (location.pathname == '/login.html') {
+            window.location.pathname = '/index.html'
+        }
+    }
+    else {
+        if (location.pathname != '/login.html') {
+            debugger                
+            window.location.pathname = '/login.html'
+            
+        }
+    }
+})
+
+var login = $("#login");
+login.click(function () {
+    $("#spinner").toggle()
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        console.log(result);
+        $("#spinner").toggle()
+
+    }).catch(function (error) {
+
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode + '\n' + errorMessage);
+        $("#spinner").toggle()
+
+    })
+});
+
+var logout = $("#Logout");
+logout.click(() => {
+    firebase.auth().signOut().catch(function (error) {
+        console.log(error);
+    })
+});
